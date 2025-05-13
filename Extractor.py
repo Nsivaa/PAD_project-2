97e726d24a86708faa7e1e39a69eb7c4843f5fed
Nsivaa <alberto.dicembre@gmail.com>
2025-05-13 18:11:31 +0100

    fixed (it seems?) scp

--- Extractor.py
+++ Extractor.py
@@ -20,35 +20,54 @@ class Extractor:
         self.n_max = n_max
         self.n_grams = defaultdict(NGramData)
         self.find_n_grams(limit) 
+        self.total_ngrams = defaultdict(int) # to count the total number of n-grams of each size
+        self.find_total_ngrams()
 
     class GluesEnum(Enum):
         scp = auto()
         dice = auto()
         phi_square = auto()
 
+    def find_total_ngrams(self):
+        """
+        Finds the total number of n-grams of each size in the corpus and stores them in the total_ngrams attribute.
+        """
+        for ngram, data in self.n_grams.items():
+            self.total_ngrams[len(ngram)] += data.frequency
 
     def find_frequency(self, ngram_words):
         data = self.n_grams.get(ngram_words)
         return data.frequency if data else 0
 
+    def find_probability(self, ngram_words):
+        data = self.n_grams.get(ngram_words)
+        if not data:
+            return 0
+        total = self.total_ngrams[len(ngram_words)]
+        return data.frequency / sum(self.total_ngrams.values())
+
     def calculate_scp(self, ngram_words, ngram_data):
-        try:
-            n_minus_one = (len(ngram_words)-1)
-            # if it's a 2-gram
-            if n_minus_one == 1:
-                denominator = self.find_frequency(ngram_words[0]) * self.find_frequency(ngram_words[1])
-                if denominator == 0:
-                    return
-                ngram_data.scp = (ngram_data.frequency**2) / denominator
-            # if it's a 3-gram or more
-            else:
-                F = 0
-                for i in range(1, n_minus_one):
-                    F += self.find_frequency(ngram_words[:i]) * self.find_frequency(ngram_words[i:])
-                F /= (n_minus_one)
-                ngram_data.scp = (ngram_data.frequency)**2 / F
-        except ZeroDivisionError:
-            pass    
+        n = len(ngram_words)
+        if n == 2:
+            p1 = self.find_probability((ngram_words[0],))
+            p2 = self.find_probability((ngram_words[1],))
+            p12 = self.find_probability(ngram_words)
+            denom = p1 * p2
+            if denom > 0:
+                ngram_data.scp = (p12 ** 2) / denom
+            return
+
+        # For n > 2
+        F = 0
+        for i in range(1, n): # range (1, n) goes up to n - 1
+            p_left = self.find_probability(ngram_words[:i])
+            p_right = self.find_probability(ngram_words[i:])
+            F += p_left * p_right
+        F /= (n - 1)
+        p_full = self.find_probability(ngram_words)
+        if F > 0:
+            ngram_data.scp = (p_full ** 2) / F
+
 
     def calculate_dice(self, ngram_words, ngram_data):
         pass
@@ -128,4 +147,16 @@ class Extractor:
         self.sort_by_glue(glue)
         # print the top n n-grams
         for i, (words, data) in enumerate(list(self.n_grams.items())[:n]):
+            print(f"{i + 1}: {words} : {data}")
+    
+    def print_bottom_n_glue(self, n: int = 10, glue: GluesEnum = GluesEnum.scp):
+        """
+        Prints the bottom n n-grams by the glue function specified in the glue parameter.
+        """
+        if glue not in self.GLUE_FUNCTIONS:
+            raise ValueError(f"Invalid glue type: {glue}")
+        # sort the n-grams by the glue function
+        self.sort_by_glue(glue)
+        # print the bottom n n-grams
+        for i, (words, data) in enumerate(list(self.n_grams.items())[-n:]):
             print(f"{i + 1}: {words} : {data}")
